915418f6b4867ae2163883534ddc943574642ac6
subhamhada <subhamrao0011@gmail.com>
2024-08-02 01:03:55 +0530

    Add files via upload

---
+++ exp6F.cpp
@@ -0,0 +1,14 @@
+//subham
+//23070123132
+//entc B2
+//experiment 6
+#include<iostream>
+using namespace std;
+int main() 
+{
+    int input = 1;
+    while (input <= 15) {
+        std::cout << "\n" << input;
+        input++;
+    }
+}
