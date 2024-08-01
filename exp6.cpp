3b25d89eba78b7e11ca1098b366b26c561c00c99
subhamhada <subhamrao0011@gmail.com>
2024-08-01 21:00:30 +0530

    Add files via upload

---
+++ exp6.cpp
@@ -0,0 +1,14 @@
+//subham
+//23070123132
+//entc B2
+//experiment 6
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i;
+    for(i=0;i<10;i++)
+    {
+        cout<<"heloo world("<<i+1<<")"<<endl;
+    }
+}
---
+++ exp6D.cpp
@@ -0,0 +1,18 @@
+//subham
+//23070123132
+//entc B2
+//experiment 6
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int number = 1;
+    
+    do {
+        cout << number << endl;
+        number++;
+    } while (number <= 10);
+
+    return 0;
+}
